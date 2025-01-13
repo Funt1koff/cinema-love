00b20f754deb0afdfde14a53f7bcb011092dc163
Sergey Funtikov <sfuntikoff@gmail.com>
2025-01-13 16:06:33 +0400

    Initial commit

---
+++ src/main/kotlin/com/funtikov/lcinema/CinemaLoveApplication.kt
@@ -0,0 +1,11 @@
+package com.funtikov.lcinema
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CinemaLoveApplication
+
+fun main(args: Array<String>) {
+	runApplication<CinemaLoveApplication>(*args)
+}
---
+++ src/test/kotlin/com/funtikov/lcinema/CinemaLoveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.funtikov.lcinema
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CinemaLoveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
