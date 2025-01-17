4dea90b40f4b621d973bf29f47f0166fb573fb40
Sergey Funtikov <sfuntikoff@gmail.com>
2025-01-17 10:44:25 +0400

    refactor layers

--- src/main/kotlin/com/funtikov/lcinema/model/Genre.kt
+++
@@ -1,9 +0,0 @@
-package com.funtikov.lcinema.model
-
-abstract class Genre {
-
-    abstract var title: String
-    abstract var code: Int
-
-    abstract var metadata: Map<String, String>
-}
--- src/main/kotlin/com/funtikov/lcinema/repository/ViewerRepository.kt
+++ src/main/kotlin/com/funtikov/lcinema/repository/ViewerRepository.kt
@@ -1,6 +1,6 @@
 package com.funtikov.lcinema.repository
 
-import com.funtikov.lcinema.model.Viewer
+import com.funtikov.lcinema.entity.Viewer
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
